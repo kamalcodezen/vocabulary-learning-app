c54f665f2448a297e13d28f057d26f5a257b5a5f
kamalcodezen <kamaluddin7908@gmail.com>
2026-03-05 05:21:33 +0530

    all level data button show in display

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,18 +1,48 @@
-// API  data get and access show in display
+// API all data get and access 
 
-const displayAllLevelData = () => {
+const allLevelApiData = () => {
 
     const url = "https://openapi.programming-hero.com/api/levels/all";
 
     fetch(url)
         .then((res) => res.json())
-        .then((data) =>console.log(data.data))
+        .then((data) => displayLevelData(data.data))
+
+}
+
+allLevelApiData();
+
+
+
+const displayLevelBtn = (id) => {
+
+    // const url = `https://openapi.programming-hero.com/api/level/${id}`;
+
+    fetch(`https://openapi.programming-hero.com/api/level/${id}`)
+        .then((res) => res.json())
+        .then((data) => console.log(data.data))
 
 }
 
 
+// display all level data 
+const displayLevelData = (buttons) => {
+
+    const displayAllLevelContainer = document.getElementById("display-all-level");
+    displayAllLevelContainer.innerHTML = "";
+
+    buttons.forEach((btn) => {
+        // console.log(btn)
+
+        const divBtn = document.createElement("div")
+        divBtn.innerHTML = `
+        <button onClick=displayLevelBtn(${btn.level_no}) class="btn btn-outline btn-info">${btn.lessonName}</button>
+        `;
+        displayAllLevelContainer.append(divBtn);
+    });
 
 
 
+}
 
-displayAllLevelData();
+

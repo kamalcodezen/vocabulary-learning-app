d45ca23d8d8b2751ea7d60c541f3c1ecfb074554
kamalcodezen <kamaluddin7908@gmail.com>
2026-03-05 06:34:40 +0530

    displayAlllevel card show complete

--- javaScript/index.js
+++ javaScript/index.js
@@ -14,18 +14,55 @@ allLevelApiData();
 
 
 
-const displayLevelBtn = (id) => {
+// akhane button ke click korlam akhane aslo ase api theke data niye nicher function er bhetore  notun kore card toiri kore screeen a dekhachch
+const displayLevelBtn = async (id) => {
+    const url = `https://openapi.programming-hero.com/api/level/${id}`;
+    const res = await fetch(url)
+    const data = await res.json()
+    displayAllLevelCard(data.data);
+}
 
-    // const url = `https://openapi.programming-hero.com/api/level/${id}`;
+const displayAllLevelCard = (cards) => {
 
-    fetch(`https://openapi.programming-hero.com/api/level/${id}`)
-        .then((res) => res.json())
-        .then((data) => console.log(data.data))
+    const allCardContainer = document.getElementById("all-card-container");
+    allCardContainer.innerHTML = "";
+
+    cards.forEach((card) => {
+        const div = document.createElement("div");
+        div.innerHTML = `
+          <div class="p-4  bg-white text-center space-y-4 border border-sky-200 rounded-lg shadow h-full flex flex-col justify-between">
+                    <div class="space-y-3">
+                        <h2 class="text-2xl font-semibold ">${card.word}</h2>
+                        <p class="font-medium opacity-80">Meaning/Pronunciation</p>
+                        <p class="font-bangla text-2xl font-semibold">${card.meaning}/${card.pronunciation}</p>
+                    </div>
+
+                    <div class="flex  justify-between">
+
+                        <button
+                            class="bg-sky-100 w-10 h-10 flex items-center justify-center rounded-full cursor-pointer">
+                            <i class="fa-solid fa-circle-info hover:scale-110 transition-all duration-300"></i>
+                        </button>
+
+                        <button
+                            class="bg-sky-100 w-10 h-10 rounded-full cursor-pointer flex justify-center items-center">
+                            <i class="fa-solid fa-volume-high hover:scale-110 transition-all duration-300"></i>
+                        </button>
+
+                    </div>
+
+             </div>
+        
+        `;
+
+        allCardContainer.append(div);
+
+    });
 
 }
 
 
-// display all level data 
+// display all level data ata page reload hole ata kaj korbe abong automatic button banbe
 const displayLevelData = (buttons) => {
 
     const displayAllLevelContainer = document.getElementById("display-all-level");
@@ -40,8 +77,6 @@ const displayLevelData = (buttons) => {
         `;
         displayAllLevelContainer.append(divBtn);
     });
-
-
 
 }
 
